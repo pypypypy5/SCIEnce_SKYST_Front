eabc58bdb9d53badeecea28c86f05c99feee5885
sungahn0206@gmail.com <sungahn0206@gmail.com>
2024-03-23 23:34:47 +0900

    like, thanks

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,50 +1,44 @@
 import React from 'react';
 import './profile.css'
 import Header from "../component/Header";
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import axios from 'axios';
 import '../Forest.jpg';
 import { Button } from 'semantic-ui-react';
 import cookie from 'react-cookies';
 export default function Profile() {
-    
+    const [post, setPost] = useState();
     const style = {
         bg:{
             background: 'Forest.jpg'
         }
     }
-    let data;
-    // useEffect(()=>{
-    //     const fetchData = async () => {
-    //         data = await axios.get('http://localhost:8888/posts/dashboard', {username: 'qdrptd'}).then((res)=>{
-                
-    //             return res.data;
-    //         })
-    //         console.log(data);
-    //     }
-    //     fetchData();
-    // }, []);
     useEffect(() => {
         const fetchData = async () => {
           try {
-            const response = await axios.post('http://localhost:8888/posts/dashboard', {body: {username: 'qdrptd'}})
-            const data = response.data;
+            const response = await axios.post('http://localhost:8888/posts/dashboard', {username: 'qdrptd'}).then((res)=>{console.log(res); return(res);})
+            const data = response.data.data;
+            console.log('asdf')
             console.log(data);
+            setPost(data);
           } catch (error) {
             console.error(error);
           }
         };
-    
         fetchData();
       }, []);
-    console.log(data);
     return (
         <div>
             <Header></Header>
-            {data.map((d) => {d.title})}
-            <Button onClick={()=>{alert(data)}}/>
+            {
+            !post?<div>1</div>:
+            <div>
             <div classname = 'Mryourwriting'>
                 Username님, 당신의 글귀들은 지금까지 
+                
+            {
+                post.map((d) => {return d.topic})
+            }
                 {/* !!!!!!!!!!유저네임 불러오는 로직 추가 필요!!!!!!!!!!*/}
             </div>
     
@@ -67,7 +61,8 @@ export default function Profile() {
             <div classname = 'ThanksGave'>
                 감사를 남긴 글들
             </div>
-
+            </div>
+            }
             <div classname = 'WritingBox1'></div>{/*글박스 불러오는 로직 추가필요*/}
             <div classname = 'WritingBox2'></div>
 
--- src/routes/read.js
+++ src/routes/read.js
@@ -1,33 +1,64 @@
 import Header from '../component/Header.js';
 import './read.css'
 import Buttons from './ReadButtons.js'
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Button } from 'semantic-ui-react';
+import { useNavigate } from 'react-router-dom';
 
-
+const date = new Date();
+const year = date.getFullYear(), month = date.getMonth() + 1, day = date.getDate();
+const dateString = `${year}-${month < 10 ? '0' : ''}${month}-${day < 10 ? '0' : ''}${day}`;
+console.log('dateString', dateString)
 export default function Read(){
+    const nav = useNavigate();
+    const [post, setPost] = useState();
+    useEffect(() => {
+        const fetchData = async () => {
+          try {
+            const response = await axios.post(`http://localhost:8888/posts/next`, {date: dateString}).then((res)=>{console.log(res.data); return(res)})
+            console.log(response.data)
+            setPost(response.data.data)
+            
+          } catch (error) {
+            console.error('Error fetching data:', error);
+          }
+        };
+    
+        fetchData(); // Call the async function
+      }, []);
+    console.log(post)
     return(
         <div>
             <Header></Header>
-
+            {/* {
+                !post?<div>1</div>:
+                post.topic
+            } */}
+            {
+            !post?<div>1</div>:
+            <div>
             <div className='rectangle' style={{marginTop: Math.max(50, 10 * window.innerHeight / 100) + 'px'}}>
                 <div className='topic'>
-                    Topic : {/*topic*/}
+                Topic : {post.topic}
                 </div>
 
                 <div className='main-content'>
-                    df
-                    {/*main content*/}
+                   { post.content}
                 </div>
 
-                    <Buttons></Buttons>
+                <Buttons></Buttons>
 
-
-                </div>
- 
-                <a href={'http://localhost:3000/read/'+String(nextaddress)}> 
-                    <img src={NextButton} style={{width: Math.max(50)}}/>  
-                </a>
 
             </div>
+
+            <Button onClick={async ()=>{
+                const response = await axios.post(`http://localhost:8888/posts/next`, {date: dateString}).then((res)=>{console.log(res.data); return(res)})
+                console.log(response.data)
+                setPost(response.data.data)
+            }}>다음</Button>
+            </div>
+            
             }
         </div>
     )
