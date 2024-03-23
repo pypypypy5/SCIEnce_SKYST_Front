55d1b64782bec2772cf3e523f093a24fa6720801
qdrptd <qdrptd@qdrptdui-MacBookPro.local>
2024-03-23 14:40:21 +0900

    first commit

---
+++ src/firebaseconfig.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCYUmflXpYNsR-8aLYt1ZVFTA3VWIKYw3I",
+  authDomain: "love-write.firebaseapp.com",
+  projectId: "love-write",
+  storageBucket: "love-write.appspot.com",
+  messagingSenderId: "544453815571",
+  appId: "1:544453815571:web:fda52a1142ec08acfabee6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import 'semantic-ui-css/semantic.min.css';
+
+
+
+
+
+const root = document.getElementById('root');
+
+ReactDOM.createRoot(root).render(
+    <div>1</div>
+//   <BrowserRouter>
+//     <Routes>
+//       <Route path="/" element={<Main />} />
+//     </Routes>
+//   </BrowserRouter>
+);
+
