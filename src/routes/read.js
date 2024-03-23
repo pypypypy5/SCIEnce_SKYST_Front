c99312544dbc4e4925a0adc096761cb4527de660
sungahn0206@gmail.com <sungahn0206@gmail.com>
2024-03-23 20:09:20 +0900

    read.js

--- src/routes/read.js
+++ src/routes/read.js
@@ -1,11 +1,31 @@
 import Header from '../component/Header.js';
+import './read.css'
+import thumbsup from './images/thumbsup.png'
+import heart from './images/heart.png'
 
 
 export default function Read(){
     return(
-        <h1>
-        <Header></Header>
-        
-        </h1>
+        <div>
+            <Header></Header>
+
+            <div className='rectangle' style={{marginTop: Math.max(50, 10 * window.innerHeight / 100) + 'px'}}>
+                <div className='topic'>
+                    Topic : {/*topic*/}
+                </div>
+
+                <div className='main-content'>
+                    df
+                    {/*main content*/}
+                </div>
+
+                <div className='buttons'>
+                        <img src={thumbsup} className='imagebutton'/>
+                        
+                        <img src={heart} className='imagebutton'/>
+                    
+                </div>
+            </div>
+        </div>
     )
 }
