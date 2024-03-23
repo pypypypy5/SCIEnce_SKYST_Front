fd3b4cf35628e97940dba433dc45111419887d5c
sungahn0206@gmail.com <sungahn0206@gmail.com>
2024-03-23 20:59:23 +0900

    read.js button

--- src/routes/ReadButtons.js
+++ src/routes/ReadButtons.js
@@ -5,26 +5,42 @@ import './readbuttons.css'
 export default function Buttons(){
     const currentTime = new Date().getHours(); /* currentTime이랑 출력하게 될 글의 작성 시간이랑 비교.*/
 
+    const thumbsupMode = 0
+    const heartMode = 0
+    const handleThumbsupClick = () => {
+        // ThumbsupClick수를 1 증가시킴 / 감소시킴
+        // class에 thumbsupClicked를 추가 / 제거
+
+    }
+
+    const handleHeartClick = () => {
+        // HeartClick수를 1 증가시킴 / 감소시킴
+        // class에 heartClicked를 추가 / 제거
+
+    }
     if (1<2) { /* 만일 currentTime이 너무 늦어서 글을 평가하면 안되는 경우 : */
         return (
             <div className = 'buttons'>
                 <div className = 'image-text-wrapper'>
                     <img src={thumbsup} class = 'unvotable-image'/>
-                    <div>To late</div>
-                    {/* thumbsup 수 */}
+                    <div>t{/* thumbsup 수 */}</div>
                 </div>
                 <div className = 'image-text-wrapper'>
                     <img src={heart} class = 'unvotable-image'/>
-                    <div>To late</div>
-                    {/* heart 수 */}
+                    <div>h{/* heart 수 */}</div>
                 </div>
             </div>
         )
     } else {
         return (
             <div className = 'buttons'>
-                <img src={thumbsup} class = 'votable-image'/>
-                <img src={heart} class='votable-image'/>
+                <div className = 'image-text-wrapper'>
+                    <img src={thumbsup} class = 'votable-image' onClick={handleThumbsupClick}/>
+                </div>
+
+                <div className = 'image-text-wrapper'>
+                    <img src={heart} class='votable-image' onClick={handleHeartClick}/>
+                </div>
             </div>
         )
     }
